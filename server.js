22e6149f940af9c47754d4ffa07a53f0c91cdde2
gersonPE <74330953+gersonPE@users.noreply.github.com>
2025-12-03 11:28:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,15 @@ app.use(cors({
 }));
 app.options("*", cors());
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://lupebeauty.com");
+  res.header("Access-Control-Allow-Origin", "https://admin.shopify.com");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
